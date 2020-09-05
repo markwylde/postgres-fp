5b1eca7ff2d7e3474283aef71c07f8f620c09e3a
Mark Wylde <me@markwylde.com>
2020-09-05 14:03:50 +1000

    Wrap tableName in quotes

--- insert.js
+++ insert.js
@@ -2,7 +2,7 @@ function objectToExecute (tableName, object) {
   const fields = Object.keys(object).map(field => `"${field}"`).join(', ');
   const parameters = Object.values(object);
   return {
-    sql: `INSERT INTO ${tableName} (${fields}) VALUES (${parameters.map((_, index) => '$' + (index + 1))})`,
+    sql: `INSERT INTO "${tableName}" (${fields}) VALUES (${parameters.map((_, index) => '$' + (index + 1))})`,
     parameters
   };
 }
